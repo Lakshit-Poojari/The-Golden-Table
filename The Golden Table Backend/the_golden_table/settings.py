1fca0f4258b051c83144d46c94b82e0f632d15d0
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-19 21:30:27 +0530

    fix middleware

--- The Golden Table Backend/the_golden_table/settings.py
+++ The Golden Table Backend/the_golden_table/settings.py
@@ -47,9 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-      "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
-   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
